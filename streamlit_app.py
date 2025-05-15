9d73bc5523443137e7afcdc31ded850d687ca7d2
rofi222 <m.rofi0604@gmail.com>
2025-05-15 16:54:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Selamat datang di website informatika")
+st.image("View/IMG_2025-04-20-17-29-29-053.jpg",width=200)
+st.write("\n")
+st.subheader("Muhammad 'Mister' Rofi")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Mari latihan membuat website melalui Github dan Streamlit bersama Mister Rofi"
 )
