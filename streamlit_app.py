f6b732e289e0b816ec062ab7f74124c2c8747d36
rofi222 <m.rofi0604@gmail.com>
2025-05-15 18:37:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+st.image("Views/IMG-20250507-WA0009.jpg",width=200)
+st.write("\n")
+st.subheader("Rofi mantap")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
